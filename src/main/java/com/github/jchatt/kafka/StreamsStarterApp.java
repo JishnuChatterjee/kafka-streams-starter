8390cee866d2f3f8bd6e368ad8dcbb1dbcec0b49
Jishnu Chatterjee <jishnu@localhost.localdomain>
2019-06-27 01:44:39 -0600

    kafka-streams

--- src/main/java/com/github/jchatt/kafka/StreamsStarterApp.java
+++ src/main/java/com/github/jchatt/kafka/StreamsStarterApp.java
@@ -1,7 +1,9 @@
 package com.github.jchatt.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
@@ -13,6 +15,8 @@ import java.util.Properties;
 public class StreamsStarterApp {
 
     public static void main(String[] args) {
+
+        System.out.println("Starting App Kafka-Streams");
         Properties properties=new Properties();
         properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streams-starter-app");
         properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -21,15 +25,23 @@ public class StreamsStarterApp {
         properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass().getName());
 
         StreamsBuilder builder=new StreamsBuilder();
+        System.out.println("Created Builder for App Kafka-Streams");
+        
+
         KStream<String,String> wordCountInput=builder.stream("word-count-input");
+
+        System.out.println("Created Kstream for word-count-input topic");
         KTable<String,Long> wordCounts=wordCountInput.mapValues(value->value.toLowerCase())
                         .flatMapValues(value-> Arrays.asList(value.split(" ")))
                         .selectKey((key,value)->value)
                         .groupByKey()
                         .count();
+        System.out.println("Created kTable for word-count-input topic");
+        wordCounts.toStream().to("word-count-output");
 
+        KafkaStreams kafkaStreams=new KafkaStreams(builder.build(), properties);
 
-
+        kafkaStreams.start();
     }
 
 }
